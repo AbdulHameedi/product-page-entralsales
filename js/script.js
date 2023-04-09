e5c63c6791d5e4306b814883f2df4045eae8b10a
AbdulHameedi <sanilinity507@gmail.com>
2023-04-09 09:03:15 +0100

    more services added

--- js/script.js
+++ js/script.js
@@ -99,7 +99,7 @@ function eagleClickTwo(){
     document.querySelector(".overlay").classList.add("eagle-display");
     document.getElementById("text-long-quickview").innerHTML = "Onions are a good source of several B vitamins, including pyridoxine."
     document.getElementById("text-short-quickview").innerHTML = "Onions can be stored in a cool, dry place for several weeks, making them a long-lasting staple";
-    document.getElementById("amount-quickview").innerHTML = '19, 000.00';
+    document.getElementById("amount-quickview").innerHTML = '<span class="line-through text-[#999999]">19,000</span> &nbsp; <i class="fa-solid fa-naira-sign"></i> 6, 000.00';
     //  
     document.getElementById("icon-quickview").classList.remove("text-[#FFD700]")
     document.getElementById("icon-quickview").classList.remove("font-bold")
@@ -165,6 +165,7 @@ function eagleClickThree(){
     //  
     document.getElementById("icon-quickview-2").classList.add("text-[#FFD700]")
     document.getElementById("icon-quickview-2").classList.add("font-bold")
+    document.getElementById("icon-quickview-2").classList.add("ftext-[#FFD700]")
     document.getElementById("review-quickview").innerHTML = '(5) Reviews';
     document.getElementById('imgforquickview').setAttribute("src" , imgAtttributeSetFourA)
 
@@ -194,6 +195,62 @@ document.querySelector(".icon-arrow-2").addEventListener("click", imgQuickviewFo
 
 
 document.querySelector(".eagle-eye-main-3").addEventListener("click", eagleClickThree);
+
+
+
+// -----------------------------------------------------FOURTH SERVICE-----------------------
+// IMAGE TRANSLATE
+function imgTranslateFour(){
+    document.querySelector(".translate-image-4").classList.add("image-transition");
+}  
+function event(){
+    imgTranslateTwo()
+}
+document.querySelector(".translate-image-4").addEventListener("mouseover", imgTranslateFour);
+
+
+// QUICK VIEW DISPLAY
+
+function eagleClickFour(){
+
+
+    var imgAtttributeSetFourA = "./assets/img/garlic-" + 1 + ".png";
+
+    document.querySelector(".quick-eye").classList.add("eagle-display");
+    document.querySelector(".overlay").classList.add("eagle-display");
+    document.getElementById("text-long-quickview").innerHTML = "Garlic is a member of the Allium family, which also includes onions, shallots, and leeks."
+    document.getElementById("text-short-quickview").innerHTML = "Garlic can be stored at room temperature for several weeks, or in the refrigerator for up to a month.";
+    document.getElementById("amount-quickview").innerHTML = '10,000.00';
+    //  
+   
+    document.getElementById('imgforquickview').setAttribute("src" , imgAtttributeSetFourA)
+
+
+    // ICON -1
+
+
+function imgQuickviewTwo(){
+    document.querySelector(".main-img-quickview").setAttribute("src", imgAtttributeSetFourB)
+}
+
+document.querySelector(".icon-arrow").addEventListener("click", imgQuickviewTwo)
+
+
+
+// ICON - 2
+
+var imgAtttributeSetFourB = "./assets/img/garlic-" + 2 + ".png";
+
+function imgQuickviewFour(){
+    document.querySelector(".main-img-quickview").setAttribute("src", imgAtttributeSetFourA)
+}
+
+document.querySelector(".icon-arrow-2").addEventListener("click", imgQuickviewFour)
+}
+
+
+
+document.querySelector(".eagle-eye-main-4").addEventListener("click", eagleClickFour);
 
 
 
